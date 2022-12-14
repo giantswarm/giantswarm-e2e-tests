636ea9712af06fb15ac4b8e1fbe48c43e13ffcdb
Mario Nitchev <mario@giantswarm.io>
2022-12-14 13:36:57 +0200

    WIP: add clusters suite

--- clusters/clusters_suite_test.go
+++ clusters/clusters_suite_test.go
@@ -1,13 +1,23 @@
 package clusters_test
 
 import (
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
+var kubeConfigPath string
+
 func TestClusters(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Clusters Suite")
 }
+
+var _ = BeforeEach(func() {
+	kubeConfigPath = os.Getenv("E2E_KUBECONFIG_PATH")
+	if kubeConfigPath == "" {
+		Fail("E2E_KUBECONFIG_PATH env var not set")
+	}
+})
--- clusters/clusters_test.go
+++ clusters/clusters_test.go
@@ -1,12 +1,21 @@
 package clusters_test
 
 import (
+	"fmt"
+	"os/exec"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	"UNKNOWN_PACKAGE_PATH"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Clusters", func() {
-
+	It("creates a cluster", func() {
+		kubeConfigFlag := fmt.Sprintf("--kubeconfig=%s", kubeConfigPath)
+		kubectlGSCommand := exec.Command("kubectl-gs", "template", "cluster", "--provider", "capa", "--organization", "$organization", "--description", "e2e test", "--name", "e2etests", kubeConfigFlag, "--output", "/tmp/cluster.yaml")
+		session, err := gexec.Start(kubectlGSCommand, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(session.Out, "10s").Should(gbytes.Say("memphis-kiam"))
+	})
 })
