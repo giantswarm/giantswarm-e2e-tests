5daecee48a87132bb3e67fa833d46fdcd1b46369
Jose Armesto <github@armesto.net>
2022-12-14 14:19:02 +0100

    Apply app manifests, make cluster name random

--- clusters/clusters_suite_test.go
+++ clusters/clusters_suite_test.go
@@ -1,14 +1,23 @@
 package clusters_test
 
 import (
+	"math/rand"
 	"os"
 	"testing"
+	"time"
+
+	config "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-var kubeConfigPath string
+var (
+	kubeConfigPath string
+	ctrlclient     client.Client
+	err            error
+)
 
 func TestClusters(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -16,8 +25,25 @@ func TestClusters(t *testing.T) {
 }
 
 var _ = BeforeEach(func() {
+	rand.Seed(time.Now().UnixNano())
 	kubeConfigPath = os.Getenv("E2E_KUBECONFIG_PATH")
 	if kubeConfigPath == "" {
 		Fail("E2E_KUBECONFIG_PATH env var not set")
 	}
+
+	// ctrlclient, err = GetManagementClusterK8sClient()
 })
+
+func GetManagementClusterK8sClient() (client.Client, error) {
+	return client.New(config.GetConfigOrDie(), client.Options{})
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
--- clusters/clusters_test.go
+++ clusters/clusters_test.go
@@ -2,20 +2,48 @@ package clusters_test
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Clusters", func() {
 	It("creates a cluster", func() {
+		clusterName := strings.ToLower(randSeq(8))
+
+		tmpFile, err := os.CreateTemp("", fmt.Sprintf("kubectl-gs-%s-", clusterName))
+		Expect(err).NotTo(HaveOccurred())
+
 		kubeConfigFlag := fmt.Sprintf("--kubeconfig=%s", kubeConfigPath)
-		kubectlGSCommand := exec.Command("kubectl-gs", "template", "cluster", "--provider", "capa", "--organization", "$organization", "--description", "e2e test", "--name", "e2etests", kubeConfigFlag, "--output", "/tmp/cluster.yaml")
+		nameFlag := strings.ToLower(fmt.Sprintf("--name=t%s", clusterName))
+		kubectlGSCommand := exec.Command("kubectl-gs", "template", "cluster", "--provider", "capa", "--organization", "giantswarm", "--description", "e2e test", nameFlag, kubeConfigFlag, "--output", tmpFile.Name())
 		session, err := gexec.Start(kubectlGSCommand, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
-		Eventually(session.Out, "10s").Should(gbytes.Say("memphis-kiam"))
+		Eventually(session, "15s").Should(gexec.Exit(0))
+
+		applyManifestsCommand := exec.Command("kubectl", kubeConfigFlag, "apply", "-f", tmpFile.Name())
+		session, err = gexec.Start(applyManifestsCommand, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(session, "10s").Should(gexec.Exit(0))
+
+		// 	Eventually(func() error {
+		// 		var err error
+		// 		app := app2.NewCR(app2.Config{
+		// 			Name:      clusterName,
+		// 			Namespace: "org-giantswarm",
+		// 		})
+		// 		err = ctrlclient.Get(context.Background(), client.ObjectKeyFromObject(app), app)
+		// 		Expect(err).NotTo(HaveOccurred())
+		//
+		// 		if app.Status.Release.Status != "deployed" {
+		// 			return errors.New("cluster app is not 'deployed' yet")
+		// 		}
+		//
+		// 		return nil
+		// 	}).Should(Succeed())
+		// })
 	})
-})
